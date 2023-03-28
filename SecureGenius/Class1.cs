27b021612d2f82b37cc3e24b698e50a9f3d1ae68
Gohulan Somanathan <gohulan@outlook.com>
2023-03-28 13:55:27 +0530

    Add project files.

---
+++ SecureGenius/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SecureGenius
+{
+    public class Class1
+    {
+        public static string Generate(int length)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
+            var random = new RNGCryptoServiceProvider();
+
+            var password = new StringBuilder();
+            for (var i = 0; i < length; i++)
+            {
+                var bytes = new byte[4];
+                random.GetBytes(bytes);
+                var randomNumber = BitConverter.ToInt32(bytes, 0);
+                var randomChar = validChars[randomNumber % validChars.Length];
+                password.Append(randomChar);
+            }
+
+            return password.ToString();
+        }
+    }
+}
